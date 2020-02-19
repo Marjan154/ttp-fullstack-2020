a4f04550a6dcef7cbb96c97701ae74c1cf49bc0b
MarjanGit <mansar16@huntersoe.org>
2020-02-19 11:57:54 -0500

    asdfasdf

--- models/db.js
+++ models/db.js
@@ -1,15 +1,2 @@
 const Sequelize = require("sequelize");
-module.exports = new Sequelize("stockportfolio", "root", "password", {
-  host: "localhost",
-  dialect: "postgres",
-  operatorsAliases: false,
-  port: 5439,
-  ssl: false,
-  timezone: "-04:00",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+module.exports = new Sequelize(process.env.DATABASE_URL);
